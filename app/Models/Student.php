5c7dd9b2a5c02984116f2db56b432152622d6c14
Shadowkibo <amitmojumder356@gmail.com>
2020-09-28 10:53:06 +0600

    Student View Fix

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -1,5 +1,5 @@
-<?php 
- 
+<?php
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -11,13 +11,18 @@ class Student extends Model
     protected $table = "students";
     protected $primaryKey = "student_id";
     protected $fillable = ["student_admission_number","student_roll_number","class_name","section_name","student_name","student_mothers_name","student_fathers_name","student_birthday_date","student_admition_date","gender_name","blood_name","category_name","religion_name","student_phone","student_email","student_height","student_weight","student_current_address","student_permanent_address","student_image","student_birth_certificate","student_marksheet","student_testimonial","student_registration_card","student_guardian_relation","student_guardian_name","student_guardian_phone","student_guardian_email","student_guardian_occupation","student_guardian_address","student_guardian_image","student_guardian_idcard","status"];
+
     public function className()
     {
-        return $this->belongsTo(ClassName::class, "class_name");
+        return $this->belongsTo("App\Models\ClassName", "class_name");
     }
     public function sectionName()
     {
-        return $this->belongsTo(SectionName::class, "section_name");
+        return $this->belongsTo("App\Models\SectionName", "section_name");
+    }
+    public function catagoryName()
+    {
+        return $this->belongsTo("App\Models\CategoryNameModel", "category_name");
     }
     public function scopeActive($query)
     {
